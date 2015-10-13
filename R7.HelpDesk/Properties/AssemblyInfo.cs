06514f78f730962abd07564c844e58e346a83aeb
roman-yagodin <roman.yagodin@gmail.com>
2015-10-13 14:43:24 +0400

    Set solution/project version to 1.0.0, add missing AssemblyInfo file, set dependency on DNN 7.4.2

---
+++ R7.HelpDesk/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("R7.HelpDesk")]
+[assembly: AssemblyDescription("Help desk (support desk, ticket tracker) module for DNN Platform, based on ADefHelpDesk code")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("R7.Solutions")]
+[assembly: AssemblyProduct("R7.HelpDesk")]
+[assembly: AssemblyCopyright("Roman M. Yagodin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
